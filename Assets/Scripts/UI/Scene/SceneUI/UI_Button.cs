cdce2c31f3193a1a917c920c5c118710a60b2517
heewoung-lee <hiwoong12@naver.com>
2024-12-22 20:15:53 +0900

    update

--- Assets/Scripts/UI/Scene/SceneUI/UI_Button.cs
+++ Assets/Scripts/UI/Scene/SceneUI/UI_Button.cs
@@ -69,7 +69,7 @@ public class UI_Button : UI_Scene
         Debug.Log($"플레이어 레벨{_playerStats.Level}");
 
         _playerStats.Gold += 5;
-        _playerStats.OnAttacked(_playerStats,80);
+        _playerStats.OnAttacked(_playerStats,1);
     }
 
 
