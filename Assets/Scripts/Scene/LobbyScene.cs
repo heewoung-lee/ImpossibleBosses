8f0dc7453eb133bff0229942e01a264a67c94bd3
heewoung-lee <hiwoong12@naver.com>
2025-01-13 21:43:00 +0900

    Lobby Editing..
    로비 만드는중

---
+++ Assets/Scripts/Scene/LobbyScene.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class LobbyScene : BaseScene
+{
+    public override Define.Scene CurrentScene => Define.Scene.LobbyScene;
+    public override void Clear()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void AwakeInit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
