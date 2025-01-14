95e2392084d1a210d25d1730d652f7654b469ae7
heewoung-lee <hiwoong12@naver.com>
2025-01-14 10:23:36 +0900

    Fixed Error
    Unity.PlasticSCM이 NUll로 뜨는 에러 수정
    Unity 패키지에서 Version Control을 지우고 다시 설치하면 정상작동

--- Assets/Scripts/Scene/LobbyScene.cs
+++ Assets/Scripts/Scene/LobbyScene.cs
@@ -5,12 +5,11 @@ public class LobbyScene : BaseScene
     public override Define.Scene CurrentScene => Define.Scene.LobbyScene;
     public override void Clear()
     {
-        throw new System.NotImplementedException();
     }
 
     protected override void AwakeInit()
     {
-        throw new System.NotImplementedException();
+
     }
 
 }
