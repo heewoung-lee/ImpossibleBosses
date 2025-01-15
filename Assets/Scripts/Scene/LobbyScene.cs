d9b94df2592b39128a0b06fc6feb994d47100d13
heewoung-lee <hiwoong12@naver.com>
2025-01-15 21:42:06 +0900

    CreateRoom Update

--- Assets/Scripts/Scene/LobbyScene.cs
+++ Assets/Scripts/Scene/LobbyScene.cs
@@ -7,10 +7,6 @@ public class LobbyScene : BaseScene
 {
     public override Define.Scene CurrentScene => Define.Scene.LobbyScene;
     UI_LobbyScene _uI_LobbyScene;
-    NetworkManager _netWorkManager;
-    UnityTransport _unityTransprot;
-
-    UI_CreateRoom _ui_createRoom;
     public override void Clear()
     {
     }
@@ -23,8 +19,6 @@ public class LobbyScene : BaseScene
     {
         base.StartInit();
         _uI_LobbyScene = Managers.UI_Manager.ShowSceneUI<UI_LobbyScene>();
-        _ui_createRoom = Managers.UI_Manager.ShowUIPopupUI<UI_CreateRoom>();
-        Managers.UI_Manager.ShowPopupUI(_ui_createRoom);
     }
 
 }
--- Assets/Scripts/UI/Scene/SceneUI/UI_LobbyScene.cs
+++ Assets/Scripts/UI/Scene/SceneUI/UI_LobbyScene.cs
@@ -1,5 +1,42 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_LobbyScene : UI_Scene
 {
+    enum Buttons
+    {
+        CreateRoomButton,
+        LoginSceneBackButton
+    }
+
+    Button _createRoomButton;
+    Button _loginSceneBackButton;
+
+    UI_CreateRoom _createRoomUI;
+
+    protected override void AwakeInit()
+    {
+        base.AwakeInit();
+        Bind<Button>(typeof(Buttons));
+        _createRoomButton = Get<Button>((int)Buttons.CreateRoomButton);
+        _createRoomButton.onClick.AddListener(ShowCreateRoomUI);
+        _loginSceneBackButton = Get<Button>((int)Buttons.LoginSceneBackButton);
+
+
+    }
+
+    public void ShowCreateRoomUI()
+    {
+        if(_createRoomUI == null)
+        {
+            _createRoomUI = Managers.UI_Manager.ShowUIPopupUI<UI_CreateRoom>();
+        }
+        Managers.UI_Manager.ShowPopupUI(_createRoomUI);
+    }
+
+    protected override void StartInit()
+    {
+        base.StartInit();
+    }
+
 }
