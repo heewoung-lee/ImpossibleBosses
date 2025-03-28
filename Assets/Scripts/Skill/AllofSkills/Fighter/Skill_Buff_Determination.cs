baa41c153535e15d4944ed2149f1180c35743739
heewoung-lee <hiwoong12@naver.com>
2025-03-28 11:33:00 +0900

    update Buff Determination
     Buff Determination 멀티 수정완료

--- Assets/Scripts/Skill/AllofSkills/Fighter/Skill_Buff_Determination.cs
+++ Assets/Scripts/Skill/AllofSkills/Fighter/Skill_Buff_Determination.cs
@@ -5,7 +5,7 @@ public class Skill_Buff_Determination : Skill_Duration
 {
     public Skill_Buff_Determination()
     {
-        _buffIconImage = Managers.ResourceManager.Load<Sprite>("Art/Player/SkillICon/WarriorSkill/BuffSkillIcon/IconSet_Equip_Helmet");
+        _buffIconImage = Managers.ResourceManager.Load<Sprite>(Buff_IconImage_Path);
         _determination = new Buffer_Determination(_buffIconImage);
     }
     //TODO: 버프 아이콘 이미지 바꿀것
@@ -14,7 +14,7 @@ public class Skill_Buff_Determination : Skill_Duration
     private Collider[] _players = null;
     private BaseController _playerController;
     private Module_Fighter_Class _fighter_Class;
-    public override string Buff_IconImage_Path => "Art/Player/SkillICon/WarriorSkill/SkillIcon/Determination";
+    public override string Buff_IconImage_Path => "Art/Player/SkillICon/WarriorSkill/BuffSkillIcon/IconSet_Equip_Helmet";
     public override float SkillDuration => 10f;
     public override Sprite BuffIconImage => _buffIconImage;
     public override Define.PlayerClass PlayerClass => Define.PlayerClass.Fighter;
@@ -22,7 +22,7 @@ public class Skill_Buff_Determination : Skill_Duration
     public override float CoolTime => 5f;
     public override string EffectDescriptionText => $"파티원들에게 10의 방어력을 부여합니다";
     public override string ETCDescriptionText => "서로간의 결의";
-    public override Sprite SkillconImage => Managers.ResourceManager.Load<Sprite>(Buff_IconImage_Path);
+    public override Sprite SkillconImage => Managers.ResourceManager.Load<Sprite>("Art/Player/SkillICon/WarriorSkill/SkillIcon/Determination");
     public override float Value => 10f;
     public override Buff_Modifier Buff_Modifier => _determination;
     public override BaseController PlayerController { 
