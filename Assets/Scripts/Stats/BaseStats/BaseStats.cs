d228d15cc3a7cfb90cf363f50f6df74331f7df36
heewoung-lee <hiwoong12@naver.com>
2025-04-04 21:23:02 +0900

    update

--- Assets/Scripts/Stats/BaseStats/BaseStats.cs
+++ Assets/Scripts/Stats/BaseStats/BaseStats.cs
@@ -55,22 +55,16 @@ public abstract class BaseStats : NetworkBehaviour, IDamageable
     public Action<float> MoveSpeedValueChangedEvent;
 
 
-    [SerializeField]
     private NetworkVariable<CharacterBaseStat> _characterBaseStatValue = new NetworkVariable<CharacterBaseStat>
          (new CharacterBaseStat(0,0,0,0,0f), NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
-    [SerializeField]
     private NetworkVariable<int> _characterHpValue = new NetworkVariable<int>
         (0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
-    [SerializeField]
     private NetworkVariable<int> _characterMaxHpValue = new NetworkVariable<int>
         (0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
-    [SerializeField]
     private NetworkVariable<int> _characterAttackValue = new NetworkVariable<int>
         (0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
-    [SerializeField]
     private NetworkVariable<int> _characterDefenceValue = new NetworkVariable<int>
        (0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
-    [SerializeField]
     private NetworkVariable<float> _characterMoveSpeedValue = new NetworkVariable<float>
        (0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
 
